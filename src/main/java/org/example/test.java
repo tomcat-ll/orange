5706b96bcd2d3e1e94b0ad93f6dcfbb8f9f70f9f
李磊 <1205710239@qq.com>
2020-12-04 16:22:34 +0800

    首次提交11111

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App {
+public class test {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(test.class,args);
     }
 }
